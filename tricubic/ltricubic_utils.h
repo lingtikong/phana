1198d02c10c35c59f61141b73ba5d3ca5b6d2b9b
Axel Kohlmeyer <akohlmey@gmail.com>
2023-04-28 12:55:05 -0400

    refactor tricubic library to follow common code conventions and add CMake support

--- tricubic/ltricubic_utils.h
+++
@@ -1,2 +0,0 @@
-int ijk2n(int i, int j, int k);
-void point2xyz(int p, int *x, int *y, int *z);
--- tricubic/tricubic.h
+++ tricubic/tricubic.h
@@ -1,7 +1,10 @@
-char *tricubic_version(void);
-void tricubic_get_coeff(double a[64], double f[8], double dfdx[8], double dfdy[8], double dfdz[8], double d2fdxdy[8], double d2fdxdz[8], double d2fdydz[8], double d3fdxdydz[8]);
-double tricubic_eval(double a[64], double x, double y, double z);
-double tricubic_eval(double a[64], double x, double y, double z, int derx, int dery, int derz);
+#ifndef TRICUBIC_H
+#define TRICUBIC_H
+extern const char *tricubic_version(void);
+extern void tricubic_get_coeff(double a[64], double f[8], double dfdx[8], double dfdy[8], double dfdz[8], double d2fdxdy[8], double d2fdxdz[8], double d2fdydz[8], double d3fdxdydz[8]);
+extern double tricubic_eval(double a[64], double x, double y, double z);
+extern double tricubic_eval(double a[64], double x, double y, double z, int derx, int dery, int derz);
 
-void tricubic_pointID2xyz(int id, int *x, int *y, int *z);
-void tricubic_pointID2xyz(int id, double *x, double *y, double *z);
+extern void tricubic_pointID2xyz(int id, int *x, int *y, int *z);
+extern void tricubic_pointID2xyz(int id, double *x, double *y, double *z);
+#endif
